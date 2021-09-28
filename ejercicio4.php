6364b87f34f7dcc4e88b7480706428d0cfff9995
Farricius <jose.hernandez.chacon.al@iespoligonosur.org>
2021-09-28 23:10:00 +0200

    try 1

---
+++ ejercicio4.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $precioPantalon = 29.99;
+    $cantidadPantalones = 3;
+    $precioCamisa = 25.99;
+    $cantidadCamisas = 2;
+    define('tarjetaCliente', 8);
+
+    $subTotal = ($precioPantalon * $cantidadPantalones) + ($precioCamisa * $cantidadCamisas);
+    $total = number_format($subTotal, 2);
+
+    $subTotalTarjeta = $total * (100 - tarjetaCliente) / 100; // (100 - 8) / 100 = 0.92 aplicando
+    $totalTarjeta = number_format($subTotalTarjeta, 2);
+
+    echo "<p>El precio de un pantalón es $precioPantalon </p>";
+    echo "<p>El precio de una camisa es $precioCamisa </p>";
+    echo "<p>El precio de la compra (sin descuento) es $total </p>";
+    echo "<p>El precio de la compra (con descuento) es $totalTarjeta </p>";
+
+    $precioZapatos = 45.99;
+    $descuentoEspecial = 12;
+    $precioZapatosCon += $precioZapatos - ($precioZapatos * ($descuentoEspecial /100));
+    echo "El precio de un zapato es de $precioZapato";
+
+    printf ("El precio de un zapato con descuento es %.2f ", %$precioZapatosCon);
+    $cuenta += $precioZapatos
+    
+
+
+
+    ?>
+</body>
+
+</html>
